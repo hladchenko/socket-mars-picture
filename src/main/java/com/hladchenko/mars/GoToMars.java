79ae561827605bd3cb2409e0213b486e9abcf6b6
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-10-11 16:44:37 +0300

    Move some logic to the SocketUtil

--- src/main/java/com/hladchenko/mars/GoToMars.java
+++ src/main/java/com/hladchenko/mars/GoToMars.java
@@ -1,49 +1,13 @@
 package com.hladchenko.mars;
 
-import lombok.SneakyThrows;
-
-import java.io.*;
-import java.net.Socket;
-
 public class GoToMars {
 
     private static final String token = "";
-    private final Socket socket;
-
-    public GoToMars(String host, int port) {
-        socket = SocketUtil.getSocket(host, port);
-    }
 
     public static void main(String[] args) {
-        GoToMars mars = new GoToMars("api.nasa.gov", 80);
-        mars.sendRequest();
-        mars.printResponse();
-    }
+        String host = "api.nasa.gov";
+        String path = "/mars-photos/api/v1/rovers/curiosity/photos?sol=15&api_key=%s".formatted(token);
 
-    @SneakyThrows
-    public void sendRequest() {
-        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
-        PrintWriter printWriter = new PrintWriter(outputStreamWriter);
-        String request = """
-                GET /mars-photos/api/v1/rovers/curiosity/photos?sol=%s HTTP/1.1
-                Host: api.nasa.gov
-                Connection: close
-                                    
-                """.formatted(token);
-        printWriter.write(request);
-        printWriter.flush();
-    }
-
-    @SneakyThrows
-    private void printResponse() {
-        InputStream inputStream = socket.getInputStream();
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        bufferedReader.lines().forEach(System.out::println);
-    }
-
-    @SneakyThrows
-    private void closeSocketConnection() {
-        socket.close();
+        SocketUtil.sendSSLRequest(host, path);
     }
 }
--- src/main/java/com/hladchenko/mars/SocketUtil.java
+++ src/main/java/com/hladchenko/mars/SocketUtil.java
@@ -2,7 +2,10 @@ package com.hladchenko.mars;
 
 import lombok.SneakyThrows;
 
+import javax.net.ssl.SSLSocketFactory;
+import java.io.*;
 import java.net.Socket;
+import java.util.List;
 
 public class SocketUtil {
 
@@ -11,9 +14,56 @@ public class SocketUtil {
     @SneakyThrows
     public static Socket getSocket(String host, int port) {
         if (socket == null) {
-            socket = new Socket(host, port);
+            socket = SSLSocketFactory.getDefault().createSocket(host, port);
         }
         return socket;
     }
 
+    @SneakyThrows
+    public static void sendSSLRequest(String host, String path) {
+        Socket socket = getSocket(host, 443);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+        PrintWriter printWriter = new PrintWriter(outputStreamWriter);
+        String request = """
+                GET %s HTTP/1.1
+                Host: %s
+                Connection: close
+                                    
+                """.formatted(path, host);
+        printWriter.write(request);
+        printWriter.flush();
+
+//        JSONObject jsonResponse = getJsonResponse();
+        printResponse();
+        closeSocketConnection();
+
+//        return jsonResponse;
+    }
+
+    @SneakyThrows
+    private static void printResponse() {
+        InputStream inputStream = socket.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        bufferedReader.lines().forEach(System.out::println);
+    }
+
+    @SneakyThrows
+    private static void getJsonResponse() {
+        InputStream inputStream = socket.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        List<String> list = bufferedReader.lines().toList();
+
+//        if (list.indexOf("") + 1 <= list.size()) {
+//            jsonObject = new JSONObject(list.get(list.indexOf("") + 1));
+//        }
+//
+//        return jsonObject;
+    }
+
+    @SneakyThrows
+    private static void closeSocketConnection() {
+        socket.close();
+    }
 }
