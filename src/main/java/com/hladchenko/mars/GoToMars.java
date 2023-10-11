537aaa115e68e355eb52aa99a49b1163619afd11
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-10-11 17:12:35 +0300

    Add Jackson object mapper

--- src/main/java/com/hladchenko/mars/GoToMars.java
+++ src/main/java/com/hladchenko/mars/GoToMars.java
@@ -1,13 +1,17 @@
 package com.hladchenko.mars;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class GoToMars {
 
-    private static final String token = "";
+    private static final String token = "vitG5CeayGcf74hW1oOr2KQcEZvoNx2useVarV8u";
 
     public static void main(String[] args) {
         String host = "api.nasa.gov";
         String path = "/mars-photos/api/v1/rovers/curiosity/photos?sol=15&api_key=%s".formatted(token);
 
-        SocketUtil.sendSSLRequest(host, path);
+        JsonNode jsonNode = SocketUtil.sendSSLRequest(host, path);
+
+        System.out.println(jsonNode);
     }
 }
